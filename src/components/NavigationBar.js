bbe58cfe6e474be53186106a6d8270b4546991b7
mia <genttunn@gmail.com>
2019-11-17 19:08:10 +0100

    nghi

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,7 @@ class NavigationBar extends Component {
     return (
       <React.Fragment>
         <Navbar bg="" expand="lg">
-          <Navbar.Brand>Mapathon</Navbar.Brand>
+          <Navbar.Brand>Mapathon Nghi</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
